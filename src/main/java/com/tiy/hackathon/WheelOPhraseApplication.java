363cc721b1dfdacf22b5c1a771baf4a32d8a76a0
Josh O'Brian <joshuaeobrian@gmail.com>
2017-04-06 13:56:19 -0400

    first commit

---
+++ src/main/java/com/tiy/hackathon/WheelOPhraseApplication.java
@@ -0,0 +1,12 @@
+package com.tiy.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelOPhraseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelOPhraseApplication.class, args);
+	}
+}
