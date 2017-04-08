3cb410647e2cf35a05f84455459a46ac96311e07
Josh O'Brian <joshuaeobrian@gmail.com>
2017-04-08 12:45:29 -0400

    pulling

--- src/main/java/com/tiy/hackathon/controller/GameRepository.java
+++ src/main/java/com/tiy/hackathon/controller/GameRepository.java
@@ -1,10 +1,23 @@
 package com.tiy.hackathon.controller;
 
+import com.tiy.hackathon.main.Phrase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by chrisaanerud on 4/7/17.
  */
+@Component
 public class GameRepository {
-    public Integer startGame(String phrase) {
-        return null;
+    @Autowired
+    JdbcTemplate template;
+
+    public void startGame(Phrase phrase) {
+
+        template.update("INSERT INTO game(category_id, datetime) VALUES( ?,now())");
+
     }
 }
